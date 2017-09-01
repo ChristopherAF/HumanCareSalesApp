4d6ce328c89edc981b2781c76e1e2e4b35f2065f
Christopher Lindblom <christopher.lindblom@afconsult.com>
2017-09-01 16:26:15 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Human Care Sales App</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />	
+		<script type="text/javascript" src="config/jquery.min.js"></script>
+		<script type="text/javascript" src="config/bootstrap.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			
+    		
+			<div class="topDiv">
+				<img src="images/logo/headerlogo.png">
+			</div>
+			<div class="contentDiv">
+	      		
+	      	</div>
+		</div>
+		
+		
+	</body>
+</html>
