0af3cedf41c5e937bb607922a214cfe790453870
Vanderpol Pierre <pierre.vanderpol@afconsult.com>
2017-09-11 17:07:24 +0200

    css file.

--- index.php
+++ index.php
@@ -1,37 +1,61 @@
-<?php
+		<?php
 
-?>
+		?>
 
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Human Care Sales App</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />	
-		<script type="text/javascript" src="config/jquery.min.js"></script>
-		<script type="text/javascript" src="config/bootstrap.js"></script>
-	</head>
-	<body>
-		<div class="container">
-			<div class="topDiv">
-				<img src="images/logo/headerlogo.png">
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Human Care Sales App</title>
+			<meta charset="utf-8">
+			<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+			<link rel="stylesheet" href="css/main.css" type="text/css" />	
+			<script type="text/javascript" src="config/jquery.min.js"></script>
+			<script type="text/javascript" src="config/bootstrap.js"></script>
+		</head>
+		<body>
+			<div class="container">
+				<div class="topDiv">
+					<img src="images/logo/headerlogo.png">
+				</div>
+				<div class="contentDiv contentDivNav">
+					<div class="cicleContainer">
+						<div class="circle-text even">
+							<a id="marketUS" href="productTypes.php?market=US">
+								<div>US</div>
+							</a>
+						</div>
+						<div class="circle-text">
+							<a id="marketNL" class="link-btn" href="productTypes.php?market=NL">
+								<div>NL</div>
+							</a>
+						</div>
+					</div>
+					<div class="cicleContainer">
+						<div class="circle-text even">
+							<a id="marketSE" class="link-btn" href="productTypes.php?market=SE">
+								<div>SE</div>
+							</a>
+						</div>
+						<div class="circle-text">
+							<a id="marketCA" class="link-btn" href="productTypes.php?market=CA">
+								<div>CA</div>
+							</a>
+						</div>
+					</div>
+					<div class="cicleContainer">	
+						<div class="circle-text even">
+							<a id="marketAU" class="link-btn" href="productTypes.php?market=AU">
+								<div>AU</div>
+							</a>
+						</div>
+						<div class="circle-text">
+							<a id="marketOM" class="link-btn" href="productTypes.php?market=OM">
+								<div>OM</div>
+							</a>
+						</div>
+					</div>
+
+				</div>
 			</div>
-			<div class="contentDiv">
-	      		<table>
-	      			<tr>
-	      				<td><a id="marketUS" href="productTypes.php?market=US">US</a></td>
-	      				<td><a id="marketNL" href="productTypes.php?market=NL">NL</a></td>
-	      			</tr>
-	      			<tr>
-	      				<td><a id="marketSE" href="productTypes.php?market=SE">SE</a></td>
-	      				<td><a id="marketCA" href="productTypes.php?market=CA">CA</a></td>
-	      			</tr>
-	      			<tr>
-	      				<td><a id="marketAU" href="productTypes.php?market=AU">AU</a></td>
-	      				<td><a id="marketOM" href="productTypes.php?market=OM">OM</a></td>
-	      			</tr>
-	      		</table>
-	      	</div>
-		</div>
-	</body>
-</html>
+		</body>
+		</html>
