e6dc1bb79bca352c10845a14120be77a1a2475a0
PIERRE VANDERPOL <pierre.vanderpol@afconsult.com>
2017-09-13 09:14:11 +0200

    index with flags CSS cleanup

--- index.php
+++ index.php
@@ -18,42 +18,92 @@
 					<img src="images/logo/headerlogo.png">
 				</div>
 				<div class="contentDiv contentDivNav">
-					<div class="cicleContainer">
-						<div class="circle-text even">
+				
+					<div class="container-img-text">
+						<div class="img-placeholder">
 							<a id="marketUS" href="productTypes.php?market=US">
+								<img  src="images/countries/us.png">
+							</a>
+							<div>US</div>
+						</div>
+						<div class="img-placeholder">
+							<a id="marketNL" href="productTypes.php?market=NL">
+								<img  src="images/countries/nl.png">	
+							</a>
+							<div>NL</div>
+						</div>
+						
+					</div>
+
+					<div class="container-img-text">
+						<div class="img-placeholder">
+							<a id="marketSE" href="productTypes.php?market=SE">
+								<img  src="images/countries/se.png">
+								<div>SE</div>
+							</a>
+						</div>
+						<div class="img-placeholder">
+							<a id="marketCA" href="productTypes.php?market=CA">
+								<img  src="images/countries/ca.png">
+								<div>CA</div>
+							</a>
+						</div>
+					</div>
+					<div class="container-img-text">
+						<div class="img-placeholder">
+							<a id="marketAU" href="productTypes.php?market=AU">
+								<img  src="images/countries/au.png">
+								<div>AU</div>
+							</a>
+						</div>
+						<div class="img-placeholder">
+							<a id="marketOM" href="productTypes.php?market=OM">
+								<img class="img-responsive img-circle" src="images/countries/om.png">
+								<div>OM</div>
+							</a>
+						</div>						
+					</div>
+
+							<!--
+
+					<div class="cicleContainer">
+						<div class="img-placeholder even">
+							<a id="marketUS" href="productTypes.php?market=US">
+								<img  src="images/countries/us.png">
 								<div>US</div>
 							</a>
 						</div>
-						<div class="circle-text">
+						<div class="img-placeholder">
 							<a id="marketNL" class="link-btn" href="productTypes.php?market=NL">
 								<div>NL</div>
 							</a>
 						</div>
 					</div>
 					<div class="cicleContainer">
-						<div class="circle-text even">
+						<div class="img-placeholder even">
 							<a id="marketSE" class="link-btn" href="productTypes.php?market=SE">
 								<div>SE</div>
 							</a>
 						</div>
-						<div class="circle-text">
+						<div class="img-placeholder">
 							<a id="marketCA" class="link-btn" href="productTypes.php?market=CA">
 								<div>CA</div>
 							</a>
 						</div>
 					</div>
 					<div class="cicleContainer">	
-						<div class="circle-text even">
+						<div class="img-placeholder even">
 							<a id="marketAU" class="link-btn" href="productTypes.php?market=AU">
 								<div>AU</div>
 							</a>
 						</div>
-						<div class="circle-text">
+						<div class="img-placeholder">
 							<a id="marketOM" class="link-btn" href="productTypes.php?market=OM">
 								<div>OM</div>
 							</a>
 						</div>
 					</div>
+					-->
 
 				</div>
 			</div>
