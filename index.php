276fd641fcdb4839c6b05cbe34ea6a1f468f82ef
PIERRE VANDERPOL <pierre.vanderpol@afconsult.com>
2017-09-22 15:55:23 +0200

    new CSS styling and HTML cleaning after test on real iPAD device.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 			<!DOCTYPE html>
 			<html>
 			<head>
+				<meta charset="utf-8">
 				<link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
 				<link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
 				<link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
@@ -20,7 +21,7 @@
 				<meta name="theme-color" content="#ffffff">
 
 				<title>Human Care Sales App</title>
-				<meta charset="utf-8">
+				
 				<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
 				<link rel="stylesheet" href="css/main.css" type="text/css" />
 				<script type="text/javascript" src="config/jquery.min.js"></script>
@@ -29,44 +30,44 @@
 			</head>
 			<body>
 				<div class="topDiv">
-						<img src="images/logo/headerlogo.png">
+						<img alt="HumanCare logo" src="images/logo/headerlogo.png">
 				</div>
 				<div class="contentDiv">
 				<div class="container">
 						<div class="row" id="market">
 							<div class="div-placeholder flag">	
 								<button id="US" name="market_btn" class="btn_img" />
-								<img src="images/countries/us.png" />
+								<img alt="Usa flag" src="images/countries/us.png" />
 								<h3>United States</h3>
 							</div>
 
 							<div class="div-placeholder  flag">	
 								<button id="NL" name="market_btn" class="btn_img"/>
-								<img src="images/countries/nl.png" />
+								<img  alt="Netherlands flag" src="images/countries/nl.png" />
 								<h3>Netherlands</h3>
 							</div>
 
 							<div class="div-placeholder  flag">
 								<button id="SE" name="market_btn" class="btn_img"/>
-								<img src="images/countries/se.png" />
+								<img alt="Swedish flag" src="images/countries/se.png" />
 								<h3>Sweden</h3>
 							</div>
 
 							<div class="div-placeholder  flag">
 								<button id="CA" name="market_btn" class="btn_img"/>
-								<img src="images/countries/ca.png" />
+								<img alt="Canadian flag" src="images/countries/ca.png" />
 								<h3>Canada</h3>
 							</div>
 
 							<div class="div-placeholder  flag">
 								<button id="AU" name="market_btn" class="btn_img"/>
-								<img src="images/countries/au.png" />
+								<img alt="Australian flag" src="images/countries/au.png" />
 								<h3>Australia</h3>
 							</div>
 
 							<div class="div-placeholder  flag">
 								<button id="OM" name="market_btn" class="btn_img"/>
-								<img src="images/countries/om.png" />
+								<img  alt="Other Markets icon" src="images/countries/om.png" />
 								<h3>Other Markets</h3>
 							</div>
 						</div><!-- end row -->
@@ -74,37 +75,37 @@
 						<div class="row" id="productTypes" style="display: none">
 							<div class="div-placeholder">	
 								<button id="MA" name="productTypes_btn" class="btn_img"/>
-								<img src="images/icons/ma.png" />
+								<img alt="Mobility Aids icon" src="images/icons/ma.png" />
 								<h3>Mobility Aids</h3>
 							</div>
 
 							<div class="div-placeholder">	
 								<button id="LS" name="productTypes_btn" class="btn_img"/>
-								<img src="images/icons/ls.png" />
+								<img  alt="Lifting Solutions icon" src="images/icons/ls.png" />
 								<h3>Lifting Solutions</h3>
 							</div>
 
 							<div class="div-placeholder">
 								<button id="HB" name="productTypes_btn" class="btn_img"/>
-								<img src="images/icons/hb.png" />
+								<img alt="Healthcare Beds icon" src="images/icons/hb.png" />
 								<h3>Healthcare Beds</h3>	
 							</div>
 
 							<div class="div-placeholder">
 								<button id="CC" name="productTypes_btn" class="btn_img"/>
-								<img src="images/icons/cc.png" />
+								<img alt="Convertible Chairs icon" src="images/icons/cc.png" />
 								<h3>Convertible Chairs</h3>	
 							</div>
 
 							<div class="div-placeholder" id="BS_productTypes" style="display: none">
 								<button id="BS" name="productTypes_btn" class="btn_img"/>
-								<img src="images/icons/bs.png" />
+								<img alt="Bathroom Safety icon" src="images/icons/bs.png" />
 								<h3>Bathroom Safety</h3>
 							</div>
 
 							<div class="div-placeholder">
 								<button id="C" name="productTypes_btn" class="btn_img"/>
-								<img src="images/icons/c.png" />
+								<img alt="Corporate icon" src="images/icons/c.png" />
 								<h3>Corporate</h3>			
 							</div>
 						</div><!-- end row-->
@@ -112,25 +113,25 @@
 						<div class="row" id="contentTypes" style="display: none">
 							<div class="div-placeholder" id="PcontentTypesContainer">
 								<button id="P" name="contentTypes_btn" class="btn_img"/>
-								<img src="images/icons/p.png" />
+								<img alt="Presentations icon" src="images/icons/p.png" />
 								<h3>Presentations</h3>			
 							</div>
 
 							<div class="div-placeholder" id="VcontentTypesContainer">
 								<button id="V" name="contentTypes_btn" class="btn_img"/>
-								<img src="images/icons/v.png" />
+								<img alt="Videos icon" src="images/icons/v.png" />
 								<h3>Videos</h3>			
 							</div>
 
 							<div class="div-placeholder" >	
 								<button id="MS" name="contentTypes_btn" class="btn_img"/>
-								<img src="images/icons/ms.png" />
+								<img  alt="Market &amp; Sales Materials icon" src="images/icons/ms.png" />
 								<h3>Market &amp; Sales Materials</h3>
 							</div>
 
 							<div class="div-placeholder">	
 								<button id="PL" name="contentTypes_btn" class="btn_img"/>
-								<img src="images/icons/pl.png" />
+								<img alt="Price List &amp; Local Content icon" src="images/icons/pl.png" />
 								<h3>Price List &amp; Local Content</h3>
 							</div>
 						</div><!-- end row-->
@@ -138,32 +139,32 @@
 						<div class="row" id="LSsubContentMarketing" style="display: none">
 							<div class="div-placeholder">	
 								<button id="QRG" name="contentTypesLS_btn" class="btn_img"/>
-								<img src="images/icons/qrg.png" />
+								<img alt="Quick Reference Guides icon" src="images/icons/qrg.png" />
 								<h3>Quick Reference Guides</h3>
 							</div>
 
 							<div class="div-placeholder">	
 								<button id="MM" name="contentTypesLS_btn" class="btn_img"/>
-								<img src="images/icons/qrg.png" />
+								<img alt="Marketing Material icon" src="images/icons/qrg.png" />
 								<h3>Marketing Material</h3>
 							</div>
 						</div><!-- end row-->
 						<div class="row" id="LSsubContentVideo" style="display: none">
 							<div class="div-placeholder clearfix">	
 								<button id="PV" name="contentTypesLS_btn" class="btn_img" />
-								<img src="images/icons/v.png" />
+								<img alt="Product Videos icon" src="images/icons/v.png" />
 								<h3>Product Videos</h3>
 							</div>
 
 							<div class="div-placeholder">	
 								<button id="TV" name="contentTypesLS_btn" class="btn_img"/>
-								<img src="images/icons/v.png" />
+								<img alt="Technical Videos icon" src="images/icons/v.png" />
 								<h3>Technical Videos</h3>
 							</div>
 
 							<div class="div-placeholder">	
 								<button id="STV" name="contentTypesLS_btn" class="btn_img"/>
-								<img src="images/icons/v.png" />
+								<img alt="Sales Training Videos icon" src="images/icons/v.png" />
 								<h3>Sales Training Videos</h3>
 							</div>
 
@@ -213,6 +214,7 @@
 						</div><!-- end row -->
 				</div><!-- end  container -->
 				</div><!-- end  contentDiv -->
+			<script  type="text/javascript" src="contentHandler.js"></script>	
 			</body>
-			<script  type="text/javascript" src="contentHandler.js"></script>
+			
 			</html>
