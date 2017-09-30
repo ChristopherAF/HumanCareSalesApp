2518f8c89890266431704dfe5ace0394a8b094d2
Christopher Lindblom <christopher.lindblom@afconsult.com>
2017-09-30 09:36:25 +0200

    Added check for if file already is offline

--- addOfflineFunction.php
+++ addOfflineFunction.php
@@ -11,10 +11,18 @@ if(isset($_POST['var_id']))
 	if (!$dbConnected) { die("Connection failed: " . mysqli_connect_error());}
 	mysqli_set_charset($dbConnected, 'utf8');
 	
-	
-	$modify_SQLselect = "INSERT INTO offline (offline.userid,offline.fileid) VALUES ('".$userid."','".$mediaId."');";
+	$offline_SQLselect = "SELECT * FROM offline WHERE userid='".$userid."'";
+	$offline_SQLselect_query = mysqli_query($dbConnected, $offline_SQLselect);
+	$fileidArray = [];
+	while ($row = mysqli_fetch_array($offline_SQLselect_query, MYSQLI_ASSOC)) {
+		array_push($fileidArray, $row['fileid']);
+	}
 
-	$modify_SQLselect_Query = mysqli_query($dbConnected, $modify_SQLselect);
+	if (!in_array($mediaId, $fileidArray)) {
+		$modify_SQLselect = "INSERT INTO offline (offline.userid,offline.fileid) VALUES ('".$userid."','".$mediaId."');";
+		$modify_SQLselect_Query = mysqli_query($dbConnected, $modify_SQLselect);
+	}
+	
 	// close conection
 	mysqli_close($dbConnected);
 }
